2b5186abed1fe61bb12a35e5b945973d71f944e5
Sam Gonzalez <61301415+sammiebelle@users.noreply.github.com>
2024-11-22 15:46:52 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    window.openMenu = function openMenu(){
+        document.getElementById("side-menu").style.display="block";
+        document.getElementById("menu-btn").style.display="none";
+        document.getElementById("close-btn").style.display="block";
+    };
+
+    window.closeMenu = function closeMenu(){
+        document.getElementById("side-menu").style.display="none";
+        document.getElementById("menu-btn").style.display="block";
+        document.getElementById("close-btn").style.display="none";
+    };
+});
